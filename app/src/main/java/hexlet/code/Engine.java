b28d3fbe2b4d2c6c3d5dd1fa289137f08291cb1f
Vasily <vasiasovet@mail.ru>
2023-11-19 17:53:25 +0300

    bigletters

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Engine {
     public static String scanName(Scanner scanner, int gameNum, String question) {
         System.out.println("Your choice: " + gameNum);
-        System.out.println("Welcome to the brain games!\nMay i have your name?");
+        System.out.println("Welcome to the Brain Games!\nMay i have your name?");
         String incomingName = scanner.next();
         System.out.println("Hello, " + incomingName + "!");
         System.out.println(question);
--- app/src/main/java/hexlet/code/games/Greetings.java
+++ app/src/main/java/hexlet/code/games/Greetings.java
@@ -6,7 +6,7 @@ public class Greetings {
     public static void great() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Your choice: 1");
-        System.out.println("Welcome to the brain games!\nMay i have your name?");
+        System.out.println("Welcome to the Brain Games!\nMay i have your name?");
         String incomingName = scanner.next();
         System.out.println("Hello, " + incomingName + "!");
         scanner.close();
