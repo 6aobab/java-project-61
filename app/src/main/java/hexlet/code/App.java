bb3c2a0abc786e7336f863e1959d33bb8f12eb6b
Vasily <vasiasovet@mail.ru>
2023-11-19 16:53:54 +0300

    addgreetings

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the brain games!");
-        Menu.openMenu();
+        //Menu.openMenu();
     }
 }
 
