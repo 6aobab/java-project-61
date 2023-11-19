35fb4755f7386020d86541d13600eeaad993402b
Vasily <vasiasovet@mail.ru>
2023-11-19 22:47:56 +0300

    corectives

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 
 public class Prime {
 
+    private static final int fountForRandomNumber = 100;
+
     public static void primeProcess() {
         Scanner scanner = new Scanner(System.in);
         String incomingName = Engine.scanName(scanner, 6, "Answer 'yes' if given number is prime."
                 + " Otherwise answer 'no'.");
-        int fountForRandomNumber = 100;
         int countToWin = 3;
         do {
 
