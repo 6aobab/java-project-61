3161ff035bd9d36092ed5d6a2da47888ff878a43
Vasily <vasiasovet@mail.ru>
2023-11-20 19:37:58 +0300

    corectives

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -15,28 +15,34 @@ public class Prime {
                 + " Otherwise answer 'no'.");
         int count = Engine.getCount();
         do {
-
-            int randomNumber = (int) (Math.random() * FACTOR_FOR_NUMBER);
-            System.out.println("Question: " + randomNumber);
-            System.out.print("Your answer: ");
-            String incomingAnswer = scanner.next();
-            if (isSimple(randomNumber) && incomingAnswer.equals("yes"))
-            {System.out.println("Correct!"); count--;
-            } else if (!isSimple(randomNumber) && incomingAnswer.equals("no"))
-            {System.out.println("Correct!"); count--;
-            } else if (isSimple(randomNumber) && incomingAnswer.equals("no")) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, "
-                        + incomingName + "!");
-                break;
-            } else if (!isSimple(randomNumber) && incomingAnswer.equals("yes")) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, "
-                        + incomingName + "!");
-                break;
-            }
-            if (count == 0) {
-                System.out.println("Congratulations, " + incomingName + "!");
-            }
+            checkSimple(scanner, count, incomingName);
         } while (count > 0);
+    }
+
+    public static int checkSimple(Scanner scanner, int count, String incomingName) {
+        int randomNumber = (int) (Math.random() * FACTOR_FOR_NUMBER);
+        System.out.println("Question: " + randomNumber);
+        System.out.print("Your answer: ");
+        String incomingAnswer = scanner.next();
+        if (isSimple(randomNumber) && incomingAnswer.equals("yes")) {
+            System.out.println("Correct!");
+            count--;
+        } else if (!isSimple(randomNumber) && incomingAnswer.equals("no")) {
+            System.out.println("Correct!");
+            count--;
+        } else if (isSimple(randomNumber) && incomingAnswer.equals("no")) {
+            System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, "
+                    + incomingName + "!");
+            count = 0;
+        } else if (!isSimple(randomNumber) && incomingAnswer.equals("yes")) {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, "
+                    + incomingName + "!");
+            count = 0;
+        }
+        if (count == 0) {
+            System.out.println("Congratulations, " + incomingName + "!");
+        }
+        return count;
     }
 
     public static boolean isSimple(Integer number) {
